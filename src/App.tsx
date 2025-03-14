8bc83d901e3e653e2a3f5440cd315751ba8b5acb
Vitor <vitorhenriquebo@yahoo.com.br>
2025-03-14 01:17:32 -0300

    adiciona componente Link ao App e estiliza com CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import Banner from "./components/Banner";
+import Link from "./components/Link";
 
 function App() {
-  return <Banner src="./Imagens/Banner Desktop.png" alt="Banner" />;
+  return (
+    <>
+      <Banner src="./Imagens/Banner Desktop.png" alt="Banner" />
+      <Link href="/" target="_blank">
+        Link
+      </Link>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Link/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Link.module.css";
+
+const Link = ({
+  children,
+  ...rest
+}: React.AnchorHTMLAttributes<HTMLAnchorElement>) => {
+  return (
+    <a className={styles.link} {...rest}>
+      {children}
+    </a>
+  );
+};
+
+export default Link;
