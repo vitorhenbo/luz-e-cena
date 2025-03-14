b56a17881675d9943484c79e2468de85fd69e476
Vitor <vitorhenriquebo@yahoo.com.br>
2025-03-14 02:42:09 -0300

    adiciona componente HeaderFormFilters com seleção de cidades e ícone

---
+++ src/components/Header/components/HeaderFormFilters.tsx
@@ -0,0 +1,32 @@
+import { FaLocationDot } from "react-icons/fa6";
+import SelectorGroup from "../../Selector/SelectorGroup";
+import SelectorOption from "../../Selector/SelectorOption";
+
+const cidades = [
+  { id: 1, nome: "São Paulo" },
+  { id: 2, nome: "Rio de Janeiro" },
+  { id: 3, nome: "Belo Horizonte" },
+  { id: 4, nome: "Curitiba" },
+  { id: 5, nome: "Porto Alegre" },
+];
+
+const HeaderFormFilters = () => {
+  return (
+    <form>
+      <SelectorGroup id="cidade" icon={<FaLocationDot />}>
+        <SelectorOption value="" label="Escolha a sua cidade" />
+        {cidades.map((cidade) => {
+          return (
+            <SelectorOption
+              key={cidade.id}
+              value={cidade.nome}
+              label={cidade.nome}
+            />
+          );
+        })}
+      </SelectorGroup>
+    </form>
+  );
+};
+
+export default HeaderFormFilters;
